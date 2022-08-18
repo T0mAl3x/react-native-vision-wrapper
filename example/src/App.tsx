8628fc0451952a63cb20cf48f196bc914ced2f51
Tom <alexandru.tomei@gmail.com>
2022-08-18 20:06:16 +0300

    Initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import { Counter } from 'react-native-vision-wrapper'
+
+const App = () => {
+  useEffect(() => {
+    console.log('')
+  })
+
+  return <Counter />
+}
+
+export default App
